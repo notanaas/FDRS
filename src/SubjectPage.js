54558706801a53fb29067e3b65204c6206271ea3
notanaas <anasalsayed3@icloud.com>
2023-09-18 14:46:08 +0300

    form on the new popup window

--- src/SubjectPage.js
+++ src/SubjectPage.js
@@ -89,51 +89,53 @@ const SubjectPage = () => {
       </button>
 
       <Modal isOpen={isModalOpen} onClose={closeModal}>
-        <h2>Upload Document or Link</h2>
-        <div>
-          <label>Select Upload Type:</label>
-          <br />
-          <input
-            type="radio"
-            id="document"
-            name="uploadType"
-            value="document"
-            checked={uploadChoice === 'document'}
-            onChange={() => handleUploadChoiceChange('document')}
-          />
-          <label htmlFor="document">Document</label>
-          <br />
-          <input
-            type="radio"
-            id="link"
-            name="uploadType"
-            value="link"
-            checked={uploadChoice === 'link'}
-            onChange={() => handleUploadChoiceChange('link')}
-          />
-          <label htmlFor="link">Link</label>
-        </div>
+  <h2>Upload Document or Link</h2>
+  <form onSubmit={handleUpload}>
+    <div>
+      <label>Select Upload Type:</label>
+      <br />
+      <input
+        type="radio"
+        id="document"
+        name="uploadType"
+        value="document"
+        checked={uploadChoice === 'document'}
+        onChange={() => handleUploadChoiceChange('document')}
+      />
+      <label htmlFor="document">Document</label>
+      <br />
+      <input
+        type="radio"
+        id="link"
+        name="uploadType"
+        value="link"
+        checked={uploadChoice === 'link'}
+        onChange={() => handleUploadChoiceChange('link')}
+      />
+      <label htmlFor="link">Link</label>
+    </div>
 
-        {uploadChoice === 'document' ? (
-          <div>
-            <label>Title:</label>
-            <input type="text" value={title} onChange={handleTitleChange} />
-            <br />
-            <label>Author:</label>
-            <input type="text" value={author} onChange={handleAuthorChange} />
-            <br />
-            <label>Choose File:</label>
-            <input type="file" />
-          </div>
-        ) : (
-          <div>
-            <label>Link:</label>
-            <input type="text" value={link} onChange={handleLinkChange} />
-          </div>
-        )}
+    {uploadChoice === 'document' ? (
+      <div>
+        <label>Title:</label>
+        <input type="text" value={title} onChange={handleTitleChange} />
+        <br />
+        <label>Author:</label>
+        <input type="text" value={author} onChange={handleAuthorChange} />
+        <br />
+        <label>Choose File:</label>
+        <input type="file" />
+      </div>
+    ) : (
+      <div>
+        <label>Link:</label>
+        <input type="text" value={link} onChange={handleLinkChange} />
+      </div>
+    )}
 
-      
-      </Modal>
+  </form>
+</Modal>
+
     </div>
   );
 };

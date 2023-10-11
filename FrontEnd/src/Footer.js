792d0bbd939b48031546a9ff0faa2216b83b316a
notanaas <anasalsayed3@icloud.com>
2023-10-11 13:17:40 +0300

    a

--- FrontEnd/src/Footer.js
+++ FrontEnd/src/Footer.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 const Footer = ({ isDarkMode, toggleDarkMode }) => {
   return (
-    <footer className="footerContainer"> 
+    <footer className={`footerContainer ${isDarkMode ? 'dark' : 'light'}`}>
       <div className="footerText">
         &copy; 2023 Your University
+      </div>
+      <div className="adminButtonContainer">
+        <Link to="/admin" className="adminButton">
+      Admin
+        </Link>
       </div>
     </footer>
   );

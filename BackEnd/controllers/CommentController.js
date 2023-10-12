899ca33a9a4b8ee869d36845e6cd096bbeac3178
leagues69 <201920074@stu.meu.edu.jo>
2023-10-12 11:50:27 +0300

    validation and sanitization on comment added

--- BackEnd/controllers/CommentController.js
+++ BackEnd/controllers/CommentController.js
@@ -1,21 +1,30 @@
 const Comment = require('../models/Comment');
+const { body, validationResult } = require("express-validator"); // validator and sanitizer
+const asyncHandler = require("express-async-handler")
+exports.comment =[
+  body("comment" , "Required text to post").trim().isLength({min:1}).escape(),
 
-exports.comment = async (req, res, next) => {
+
+  asyncHandler(async (req, res, next) => {
+  const errors = validationResult(req)
   const { text } = req.body;
   const resourceId = req.params.resourceId; // Retrieve resourceId from route parameters
-
-  try {
+  
+  if(!errors.isEmpty())
+  {
+      return res.status(403).json(
+          {
+              errors : errors.array()
+          })
+  }
     // Create a new comment and save it to the database, associating it with the specified resource
     const newComment = new Comment({
-      Comment: text,
-      Resource: resourceId,
-      user: req.user._id, // Assuming you have user information available in the request.
+    Comment: text,
+    Resource: resourceId,
+    user: req.user._id, // Assuming you have user information available in the request.
     });
     await newComment.save();
 
     // Return a success response
     return res.status(201).json({ message: 'Comment created successfully' });
-  } catch (error) {
-    return res.status(500).json({ error: 'An error occurred while creating the comment' });
-  }
-};
+})];

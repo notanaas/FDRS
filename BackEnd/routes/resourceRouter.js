f7ad8b1466ff9f5c04cecd9042d59c377a558479
leagues69 <201920074@stu.meu.edu.jo>
2023-10-12 08:40:22 +0300

    Resource detail added

--- BackEnd/routes/resourceRouter.js
+++ BackEnd/routes/resourceRouter.js
@@ -8,6 +8,8 @@ const router = express.Router();
 router.post('/resource/create' ,passport.authenticate('jwt', { session: false }),Resource.Resource_create_post
 )//Get all resource for resource page
 router.get('/resources/:FacultyName', Resource.resource_list);
+//get resource details
+router.get('/resource/:id' , Resource.Resource_detail)
 //download a resource
 router.get('/download/:fileId' , Resource.pdf_download)
 //get a search

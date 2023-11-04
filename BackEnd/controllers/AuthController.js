0e973d6cac5ef04ecfb588bf8c600778933b3d86
leagues69 <201920074@stu.meu.edu.jo>
2023-11-04 11:14:36 +0300

    email forgot password testing work

--- BackEnd/controllers/AuthController.js
+++ BackEnd/controllers/AuthController.js
@@ -148,58 +148,69 @@ exports.refresh_token = async(req,res)=>
     }
 }
 
-exports.forgot_password  = [
-    body("email" , "Email must be required")
-    .trim()
-    .isLength({min:1})
-    .escape()
-    .custom(async (Email)=>
-    {
+exports.forgot_password = [
+    body('email', 'Email must be required')
+      .trim()
+      .isLength({ min: 1 })
+      .escape()
+      .custom(async (email, { req }) => {
         try {
-            const emailExists = await Users.findOne({Email : Email})
-            if(!emailExists)
-            {
-                throw new Error('Email does not exists')
-            }
+          const emailExists = await Users.findOne({ Email: email });
+          if (!emailExists) {
+            throw new Error('Email does not exist');
+          }
         } catch (error) {
-            throw new Error(error)
+          throw new Error(error.message);
         }
-    }),
-    asyncHandler(async(req,res,next)=>{
-        const emailExists = await Users.findOne({Email : req.body.Email})
-        const secret = process.env.JWT_SECRET + emailExists.Password
+      }),
+      asyncHandler(async (req, res, next) => {
+        const emailExists = await Users.findOne({ Email: req.body.email });
+        console.log(req.body);
+        
+        if (!emailExists) {
+          // Handle the case where the email doesn't exist
+          return res.status(404).json({ message: 'Email not found' });
+        }
+  
+        const secret = process.env.JWT_SECRET + emailExists.Password;
+  
         const payload = {
-            email :  emailExists.Email,
-            id:emailExists._id
-        }
-        const token  = jwt.sign(payload,secret,{expiresIn :"20m"})
-        const link = `http://localhost:3000/reset-password/${emailExists._id}/${token}`
+          email: emailExists.Email,
+          id: emailExists._id
+        };
+        const token = jwt.sign(payload, secret, { expiresIn: '20m' });
+        const link = `http://localhost:3000/reset-password/${emailExists._id}/${token}`;
         const transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
-              user: 'wfarid31@gmail.com',
-              pass: 'yourpassword'
+              user: 'FDRS1697@gmail.com',
+              pass: process.env.pass   
             }
           });
           
           const mailOptions = {
-            from: 'wfarid31@gmail.com',
+            from: 'FDRS1697@gmail.com',
             to: 'wfarid31@gmail.com',  // emailExists
             subject: 'Reset password link',
             text: link
           };
           
-          transporter.sendMail(mailOptions, function(error, info){
+          transporter.sendMail(mailOptions, function (error, info) {
             if (error) {
-              console.log(error);
+              // Handle the error when sending the email fails
+              console.error(error); // Log the error for debugging
+              return res.status(500).json({ message: "Email sending failed. Please try again later." });
             } else {
+              // Email was sent successfully
               console.log('Email sent: ' + info.response);
+              return res.status(200).json({ message: "Password reset link has been sent to your email." });
             }
           });
-        return res.status(200).json({message:"password reset link has been sent to you're email... "})
-    })
-]   
-
+          
+      })
+  ];
+     
+ 
 exports.get_reset_password = asyncHandler(async(req,res,next)=>
 {
     const {id,token} = req.params

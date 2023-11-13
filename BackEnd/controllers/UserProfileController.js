f471d6e1923772b34e4a6b9ab87d0707138c37c0
leagues69 <201920074@stu.meu.edu.jo>
2023-11-13 16:30:13 +0300

    admin acceptence or not for resource added.

--- BackEnd/controllers/UserProfileController.js
+++ BackEnd/controllers/UserProfileController.js
@@ -1,5 +1,6 @@
 const Users = require("../models/Users")
 const UserFavs = require('../models/UserFavRes')
+const Resrouce = require('../models/Resources')
 const asyncHandler = require("express-async-handler")
 
 
@@ -16,3 +17,28 @@ exports.profile = asyncHandler(async(req,res,next)=>
     return res.status(200).json({profile:profile , userfavorites : UserFavs})
 
 })
+exports.resource_authorize = asyncHandler(async(req,res,next)=>
+{
+    const resource = await Resrouce.find({isAuthorized:false}).exec()
+    if(!resource)
+    {
+        return res.status(404).json({message:"no resources available"})
+    }
+    return res.status(200).json({resource:resource})
+})
+exports.admin_acceptance = asyncHandler(async (req, res, next) => {
+    let flag = false;
+    const btnAccept = req.body.accept;
+
+    if (flag !== btnAccept) {
+        flag = true;
+    }
+
+    // Use await to wait for the update operation to complete
+    const resource = await Resrouce.findByIdAndUpdate(req.params.id, { isAuthorized: flag }, { new: true }).exec();
+
+    // Additional code if needed
+
+    // Send a response or do something else
+    res.status(200).json({ success: true, data: resource });
+});
--- BackEnd/routes/UsersRouter.js
+++ BackEnd/routes/UsersRouter.js
@@ -3,7 +3,19 @@ const Profile = require('../controllers/UserProfileController');
 const passport = require('passport');
 const router = express.Router();
 
+const isAdmin = (req, res, next) => {
+    if (req.user && req.user.isAdmin) {
+        return next();
+    }
+    return res.status(403).json({ message: "Not authorized. Admin access required." });
+};
+
 /* GET users listing. */
 router.get('/profile',passport.authenticate('jwt', { session: false }), Profile.profile);
 
+// Your existing resource_authorize route
+app.get("/resource/authorize", passport.authenticate('jwt', { session: false }), isAdmin, resource_authorize);
+
+// Your existing admin_acceptance route
+app.post("/admin/acceptance/:id", passport.authenticate('jwt', { session: false }), isAdmin, admin_acceptance);
 module.exports = router;

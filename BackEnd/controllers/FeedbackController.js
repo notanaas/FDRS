6305859df478f06e62a4e8a3361e2d10fa2bf865
leagues69 <201920074@stu.meu.edu.jo>
2023-11-30 16:59:47 +0300

    get all feedbacks added

--- BackEnd/controllers/FeedbackController.js
+++ BackEnd/controllers/FeedbackController.js
@@ -14,3 +14,13 @@ exports.feedback = asyncHandler(async(req,res,next)=>
     return res.status(201).json({message:"Thank you for you're feedback"})
 
 })
+
+exports.get_feedbacks = asyncHandler(async(req,res,next)=>
+{
+    const feedbacks  = FeedBack.find({}).exec()
+    if(!feedbacks)
+    {
+       return res.status(404).json({message:"no feedbacks"})
+    }
+    return res.status(200).json({feedbacks:feedbacks})
+})
--- BackEnd/routes/FeedBackRouter.js
+++ BackEnd/routes/FeedBackRouter.js
@@ -3,7 +3,14 @@ const FeedBack = require('../controllers/FeedbackController');
 const passport = require('passport');
 const router = express.Router();
 
-router.post("/FeedBack-post" , passport.authenticate('jwt', { session: false }) ,FeedBack.feedback)
+const isAdmin = (req, res, next) => {
+    if (req.user && req.user.isAdmin) {
+        return next();
+    }
+    return res.status(403).json({ message: "Not authorized. Admin access required." });
+};
 
+router.post("/FeedBack-post" , passport.authenticate('jwt', { session: false }) ,FeedBack.feedback)
+router.get("/feedbacks" , passport.authenticate('jwt', { session: false }) , isAdmin , )
 
 module.exports = router;

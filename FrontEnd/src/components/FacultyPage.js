3d1afe4f1d9603da83a3ef25a1530f273d469155
notanaas <anasalsayed3@icloud.com>
2023-11-30 15:52:39 +0300

    Update FacultyPage.js

--- FrontEnd/src/components/FacultyPage.js
+++ FrontEnd/src/components/FacultyPage.js
@@ -1,9 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react';
-<<<<<<< Updated upstream
 import DocumentCard from './DocumentCard'; 
-=======
-import DocumentCard from './DocumentCard';
->>>>>>> Stashed changes
 import axios from 'axios';
 import { useParams, useHistory } from 'react-router-dom';
 import { AuthContext } from './context/AuthContext';
@@ -31,29 +27,21 @@ const FacultyPage = () => {
 
     const fetchResources = async () => {
       try {
-<<<<<<< Updated upstream
-        const resourcesResponse = await axios.get(`${backendURL}/api_resource/faculty/${facultyId}`);
-        setResources(resourcesResponse.data.resource_list);
-=======
+
         const response = await axios.get(`${backendURL}/api_resource/faculty/${facultyId}`);
         setResources(response.data.resource_list);
->>>>>>> Stashed changes
       } catch (err) {
         console.error('Error fetching resources:', err);
         setError(err.response?.data?.error || 'An error occurred while fetching resources.');
       }
     };
 
-<<<<<<< Updated upstream
     if (authToken) {
       fetchUserProfile();
       fetchResources();
     }
   }, [facultyId, authToken, backendURL]);
-=======
-    fetchResources();
-  }, [facultyId]);
->>>>>>> Stashed changes
+ 
 
   const isResourceFavorited = resourceId => userFavorites.includes(resourceId);
 
@@ -68,20 +56,6 @@ const FacultyPage = () => {
   return (
     <div>
       <h1>Resources for Faculty</h1>
-<<<<<<< Updated upstream
-      {resources.length > 0 ? (
-        resources.map(resource => (
-          <DocumentCard 
-            key={resource._id} 
-            document={resource} 
-            onClick={() => handleCardClick(resource._id)}
-            isFavorited={isResourceFavorited(resource._id)} // Pass the favorited status
-          />
-        ))
-      ) : (
-        <p>No resources found for this faculty.</p>
-      )}
-=======
       <div className="faculty-container">
         {resources.length > 0 ? (
           resources.map((resource) => (
@@ -91,7 +65,6 @@ const FacultyPage = () => {
           <p>No resources found for this faculty.</p>
         )}
       </div>
->>>>>>> Stashed changes
     </div>
   );
 };

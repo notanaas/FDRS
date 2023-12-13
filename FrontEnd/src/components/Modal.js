93741735d99daa22a13b178cf4caefa6e501404e
notanaas <anasalsayed3@icloud.com>
2023-12-13 09:49:44 +0300

    dark&&light mode

--- FrontEnd/src/components/Modal.js
+++ FrontEnd/src/components/Modal.js
@@ -2,14 +2,11 @@ import React from 'react';
 import './Header.css';
 
 const Modal = ({ isOpen, onClose, children, isDarkMode }) => {
-  const modalContentStyle = {
-    backgroundColor: isDarkMode ? 'black' : 'white',
-    color: isDarkMode ? 'white' : 'black',
-  };
+ 
 
   return (
     <div className="upload-modal" style={{ display: isOpen ? 'flex' : 'none' }} onClick={onClose}>
-      <div className="upload-modal-content" style={modalContentStyle} onClick={(e) => e.stopPropagation()}>
+      <div className="upload-modal-content" onClick={(e) => e.stopPropagation()}>
         <div className="modal-body">{children}</div>
       </div>
     </div>

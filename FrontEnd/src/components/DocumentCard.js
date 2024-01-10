63be505b136ad8b88581b894c39e9ada595ef3f5
Karborani1 <s.karborani@hotmail.com>
2024-01-10 23:35:47 +0300

    دفع

--- FrontEnd/src/components/DocumentCard.js
+++ FrontEnd/src/components/DocumentCard.js
@@ -9,7 +9,7 @@ const DocumentCard = ({ cardType, document, onClick, deleteFeedback, sendEmail, 
   const [userFavorites, setUserFavorites] = useState([]);
   const [isFavorited, setIsFavorited] = useState(document?.isFavorited || false);
   const { authToken, isLoggedIn } = useContext(AuthContext);
-  const backendURL = 'http://localhost:3002';  const [feedbacks, setFeedbacks] = useState([]);
+  const backendURL = 'http://localhost:3002'; const [feedbacks, setFeedbacks] = useState([]);
   const [showLoginPrompt, setShowLoginPrompt] = useState(false);
   const history = useHistory();
   const location = useLocation();
@@ -38,17 +38,17 @@ const DocumentCard = ({ cardType, document, onClick, deleteFeedback, sendEmail, 
     setActionError(errorMsg);
     clearTimeout(messageTimeout);
     const newTimeout = setTimeout(() => {
-        setActionSuccess('');
-        setActionError('');
+      setActionSuccess('');
+      setActionError('');
     }, 3000);
     setMessageTimeout(newTimeout);
-};
+  };
 
-useEffect(() => {
+  useEffect(() => {
     return () => {
-        clearTimeout(messageTimeout);
+      clearTimeout(messageTimeout);
     };
-}, [messageTimeout]);
+  }, [messageTimeout]);
 
   if (!document) return null;
   const goToResourceDetail = () => {
@@ -116,8 +116,8 @@ useEffect(() => {
   };
   const cardClassName = "card"; // This is the new class name for all card types
 
-  
-  
+
+
   const handleDelete = async () => {
     resetMessages();
     try {
@@ -130,35 +130,35 @@ useEffect(() => {
   const handleDeleteFeedback = async (e, feedbackId) => {
     e.stopPropagation();
     try {
-        await deleteFeedback(feedbackId);
-        setMessageWithTimer('Feedback deleted successfully.', '');
+      await deleteFeedback(feedbackId);
+      setMessageWithTimer('Feedback deleted successfully.', '');
     } catch (error) {
-        setMessageWithTimer('', 'Failed to delete feedback.');
+      setMessageWithTimer('', 'Failed to delete feedback.');
     }
-};
+  };
 
-  
+
   const handleSendEmail = (e, emailAddress) => {
     e.stopPropagation(); // Prevent event bubbling
     sendEmail(emailAddress);
   };
-  
-  
-    const stopPropagation = (e) => e.stopPropagation();
-    const CardContent = () => {
-      const cardStyle = {
-        backgroundImage: `url(${backendURL}/api_resource/cover/${document._id})`
-      };
-    
-    
-    
+
+
+  const stopPropagation = (e) => e.stopPropagation();
+  const CardContent = () => {
+    const cardStyle = {
+      backgroundImage: `url(${backendURL}/api_resource/cover/${document._id})`
+    };
+
+
+
     switch (cardType) {
       case 'adminActions':
         return (
           <div className="card" style={cardStyle} onClick={goToResourceDetail}>
-      <div className="card-content">
-        <h3 className="card-title">{document.Title || "Untitled"}</h3>
-             
+            <div className="card-content">
+              <h3 className="card-title">{document.Title || "Untitled"}</h3>
+
               <h3>Faculty: {document.Faculty?.FacultyName || "No faculty name provided"}</h3>
               <h3 className="card-uploader">Uploader: {document.User.Email}</h3>
             </div>
@@ -178,28 +178,28 @@ useEffect(() => {
           </div>
         );
 
-        case 'resource':
-          return (
-            <div className="card" style={cardStyle} onClick={goToResourceDetail}>
+      case 'resource':
+        return (
+          <div className="card" style={cardStyle} onClick={goToResourceDetail}>
             <div className="card-content">
               <h3 className="card-title">{document.Title || "Untitled"}</h3>
-              </div>
-              <div className="card-description">
-                <a href={`${backendURL}/api_resource/download/${document._id}`} target='_blank' className="downloadButton">Download</a>
-                {onDelete && (
-             <button className="trashButton" onClick={(e) => { e.stopPropagation(); onDelete(document._id); }}>
-             🗑️ 
-         </button>
-         
-            )}
-</div>
             </div>
-          );
+            <div className="card-description">
+              <a href={`${backendURL}/api_resource/download/${document._id}`} target='_blank' className="downloadButton">Download</a>
+              {onDelete && (
+                <button className="trashButton" onClick={(e) => { e.stopPropagation(); onDelete(document._id); }}>
+                  🗑️
+                </button>
+
+              )}
+            </div>
+          </div>
+        );
       case 'favorite':
         return (
           <div className="card" style={cardStyle} onClick={goToResourceDetail}>
-      <div className="card-content">
-        <h3 className="card-title">{document.Title || "Untitled"}</h3>
+            <div className="card-content">
+              <h3 className="card-title">{document.Title || "Untitled"}</h3>
             </div>
             <div className="card-description">
               <a href={`${backendURL}/api_resource/download/${document._id}`} target='_blank' className="downloadButton">Download</a>
@@ -210,74 +210,74 @@ useEffect(() => {
           </div>
         );
 
-        case 'faculty':
-          return (
-            <div className="card" style={cardStyle} onClick={goToResourceDetail}>
-              <div className="card-content">
-                <h3 className="card-title">{document.Title || "Untitled"}</h3>
-                <h3 className="card-author">Author: {document.Author_first_name || "Unknown"} {document.Author_last_name || ""}</h3>
-              </div>
-              <div className="card-description">
-                {document.Description || "No description provided"}
-                <button className="favorite-button" onClick={(e) => { e.stopPropagation(); handleFavButtonClick(); }}>
-                  {isFavorited ? '\u2605' : '\u2606'}
-                </button>
-                {showLoginPrompt && (
-                  <div className="error-message">Please log in to add to favorites.</div>
-                )}
-              </div>
+      case 'faculty':
+        return (
+          <div className="card" style={cardStyle} onClick={goToResourceDetail}>
+            <div className="card-content">
+              <h3 className="card-title">{document.Title || "Untitled"}</h3>
+              <h3 className="card-author">Author: {document.Author_first_name || "Unknown"} {document.Author_last_name || ""}</h3>
             </div>
-          );
-        
-        case 'feedback':
-          return (
-            <div  className="feedbackcard"onClick={(e) => e.stopPropagation()}> 
-              <div>
-                <h3><strong>Email:</strong> {document.User.Email}</h3>
-                <h3><strong>Feedback:</strong> {document.SearchText}</h3>
-                <div>
-                  <button 
-                    className="authButton" 
-                    onClick={(e) => handleDeleteFeedback(e, document._id)}>
-                    Delete Feedback
-                  </button>
-                  <button 
-                    className="authButton" 
-                    onClick={(e) => handleSendEmail(e, document.User.Email)}> 
-                    Send Email
-                  </button>
-                </div>
-              </div>
+            <div className="card-description">
+              {document.Description || "No description provided"}
+              <button className="favorite-button" onClick={(e) => { e.stopPropagation(); handleFavButtonClick(); }}>
+                {isFavorited ? '\u2605' : '\u2606'}
+              </button>
+              {showLoginPrompt && (
+                <div className="error-message">Please log in to add to favorites.</div>
+              )}
             </div>
-          );
-          case 'search':
-      return (
-        <div className="card search-card" style={cardStyle} onClick={goToResourceDetail}>
-          <div className="card-content">
-            <h3 className="card-title">{document.Title || "Untitled"}</h3>
-            <p className="card-author">Author: {document.Author_first_name} {document.Author_last_name}</p>
           </div>
-          <div className="card-description">
-            {document.Description || "No description provided"}
-            <button className="favorite-button" onClick={(e) => { e.stopPropagation(); handleFavButtonClick(); }}>
+        );
+
+      case 'feedback':
+        return (
+          <div className="feedbackcard" onClick={(e) => e.stopPropagation()}>
+            <div>
+              <h3><strong>Email:</strong> {document.User.Email}</h3>
+              <h3><strong>Feedback:</strong> {document.SearchText}</h3>
+              <div>
+                <button
+                  className="authButton"
+                  onClick={(e) => handleDeleteFeedback(e, document._id)}>
+                  Delete Feedback
+                </button>
+                <button
+                  className="authButton"
+                  onClick={(e) => handleSendEmail(e, document.User.Email)}>
+                  Send Email
+                </button>
+              </div>
+            </div>
+          </div>
+        );
+      case 'search':
+        return (
+          <div className="card search-card" style={cardStyle} onClick={goToResourceDetail}>
+            <div className="card-content">
+              <h3 className="card-title">{document.Title || "Untitled"}</h3>
+              <p className="card-author">Author: {document.Author_first_name} {document.Author_last_name}</p>
+            </div>
+            <div className="card-description">
+              {document.Description || "No description provided"}
+              <button className="favorite-button" onClick={(e) => { e.stopPropagation(); handleFavButtonClick(); }}>
                 {isFavorited ? '\u2605' : '\u2606'}
                 {showLoginPrompt && (
                   <span className="login-tooltip">Log in to add</span>
                 )}
               </button>
+            </div>
           </div>
-          </div>
-      );
+        );
       default:
         return null;
     }
   };
 
   return (
-<div>
-    {actionSuccess && <div className="success-message">{actionSuccess}</div>}
-    {actionError && <div className="error-message">{actionError}</div>}
-    <CardContent onDelete={handleDelete} />
+    <div>
+      {actionSuccess && <div className="success-message">{actionSuccess}</div>}
+      {actionError && <div className="error-message">{actionError}</div>}
+      <CardContent onDelete={handleDelete} />
 
 
     </div>
